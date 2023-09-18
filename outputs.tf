36ae6ce55a7eb51f8aea3f1a5d5bdd82782b8579
Kamran Biglari <kamran.biglari@newchangefx.com>
2023-09-18 12:07:19 +0100

    init

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,15 @@
+output "domain" {
+  value = var.domain
+}
+
+output "endpoint" {
+  value = var.endpoint
+}
+
 output "fqdn" {
   value = aws_route53_record.endpoint[0].fqdn
+}
+
+output "certificate_arn" {
+  value = aws_acm_certificate.this.arn
 }
